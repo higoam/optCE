c388f1607f349083eb283cadc5750bed1169de76
Higo Albuquerque <higo.f@samsung.com>
2019-02-25 17:05:38 -0400

    experimentos 25 Fev

--- Experimentos_gamma/CEGIO_F/experimentos_CEGIO_F.sh
+++ Experimentos_gamma/CEGIO_F/experimentos_CEGIO_F.sh
@@ -1,66 +1,85 @@
 #!/bin/bash
 
 declare -a functions=(
+#'adjiman.func'
+#'bohachevsky_1.func'
+#'bohachevsky_3.func'
+#'branin_RCOS.func'
+#'camel_six.func'
+#'camel_three.func'
+#'cosine.func'
+#'scahffer_1.func'
+#'styblinski_tang.func'
+#'trecanni.func'
+#'tsoulos.func'
+#'ursem_1.func'
+#'wayburn_seader_1.func'
+#'zirilli.func'
+#'rotated_ellipse.func'
+#
+#'alpine1.func'
+#'egg_crate.func'
+#'himmelblau.func'
+#'leon.func'
+#'price_4.func'
+#'schwefel_2_25.func'
+#'sphere.func'
+#'wayburn_seader_2.func'
+#'price_1.func'
+#'dixon_price.func'
+#
 'booth.func'
 'chung_reynolds.func'
 'cube.func'
-'dixon_price.func'
-'power_sum.func'
+'mccormick.func'
+'godstein_price.func'
 'schumer.func'
 'sum_squares.func'
 'matyas.func'
-'rotated_ellipse.func'
+'rotated_ellipse_02.func'
 'zettl.func'
-'mccormick.func'
-'godstein_price.func'
 );
 
-	echo
-	echo "#############  Experiments to --convex CEGIO-F prec2  ##############"	
-	echo
+	echo "#############  Experiments to --convex CEGIO-F Prec 3  ##############"	>> resultado_exp_CEGIO_G_MINISAT.txt
+	echo "#############  Experiments to --convex CEGIO-F Prec 3  ##############"	
 
-	echo "#############  CBMC - MINISAT  ##############"	
+	echo "#############  CBMC - MINISAT  ##############"	>> resultado_exp_CEGIO_G_MINISAT.txt
+	echo "#############  CBMC - MINISAT  ##############"
 	for i in "${functions[@]}"
 	do		
 		line=$i
 		func=${line//,*}       	
 
-		echo
-		(time ./optCE $func --cbmc --minisat --convex --library=math2.h --precision=2 >> resultado_exp_CEGIO_F_MINISAT.txt) 
-		echo
+		(time ./optCE_1000 $func --cbmc --minisat --convex --library=math2.h --precision=3) &>> resultado_exp_CEGIO_G_MINISAT.txt
 	done
 
+	echo "#############  ESBMC - Mathsat  ##############"	>> resultado_exp_CEGIO_G_MINISAT.txt	
 	echo "#############  ESBMC - Mathsat  ##############"	
 	for i in "${functions[@]}"
 	do		
 		line=$i
 		func=${line//,*}       	
 
-		echo
-		(time ./optCE $func --esbmc --mathsat --convex --library=math2.h --precision=2 >> resultado_exp_CEGIO_F_MATHSAT.txt) 
-		echo
+		(time ./optCE_1000 $func --esbmc --mathsat --convex --library=math2.h --precision=3) &>> resultado_exp_CEGIO_G_MATHSAT.txt 
 	done
 
-
+	echo "#############  ESBMC - Boolector  ##############"	>> resultado_exp_CEGIO_G_MINISAT.txt
 	echo "#############  ESBMC - Boolector  ##############"	
 	for i in "${functions[@]}"
 	do		
 		line=$i
 		func=${line//,*}       	
 
-		echo
-		(time ./optCE $func --esbmc --boolector --convex --library=math2.h --precision=2 >> resultado_exp_CEGIO_F_BOOLECTOR.txt) 
-		echo
+		(time ./optCE_1000 $func --esbmc --boolector --convex --library=math2.h --precision=3) &>> resultado_exp_CEGIO_G_BOOLECTOR.txt 
 	done
 
+	echo "#############  ESBMC - Z3  ##############" >> resultado_exp_CEGIO_G_MINISAT.txt
 	echo "#############  ESBMC - Z3  ##############"	
 	for i in "${functions[@]}"
 	do		
 		line=$i
 		func=${line//,*}       	
 
-		echo
-		(time ./optCE $func --esbmc --z3 --convex --library=math2.h --precision=2 >> resultado_exp_CEGIO_F_Z3.txt) 
-		echo
+		(time ./optCE_1000 $func --esbmc --z3 --convex --library=math2.h --precision=3) &>> resultado_exp_CEGIO_G_Z3.txt 
 	done
 
--- Experimentos_gamma/CEGIO_S/experimentos_CEGIO_S.sh
+++ Experimentos_gamma/CEGIO_S/experimentos_CEGIO_S.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 declare -a functions=(
+#'adjiman.func'
+#'bohachevsky_1.func'
+#'bohachevsky_3.func'
+#'branin_RCOS.func'
+#'camel_six.func'
+#'camel_three.func'
+#'cosine.func'
+#'scahffer_1.func'
+#'styblinski_tang.func'
+#'trecanni.func'
+#'tsoulos.func'
+#'ursem_1.func'
+#'wayburn_seader_1.func'
+#'zirilli.func'
+#'rotated_ellipse.func'
+#
 'alpine1.func'
 'egg_crate.func'
 'himmelblau.func'
@@ -10,54 +26,60 @@ declare -a functions=(
 'sphere.func'
 'wayburn_seader_2.func'
 'price_1.func'
+'dixon_price.func'
+#
+#'booth.func'
+#'chung_reynolds.func'
+#'cube.func'
+#'mccormick.func'
+#'godstein_price.func'
+#'schumer.func'
+#'sum_squares.func'
+#'matyas.func'
+#'rotated_ellipse_02.func'
+#'zettl.func'
 );
 
-	echo
-	echo "#############  Experiments to --positive CEGIO-S prec2  ##############"	
-	echo
+	echo "#############  Experiments to --positive CEGIO-S Prec 3  ##############"	>> resultado_exp_CEGIO_G_MINISAT.txt
+	echo "#############  Experiments to --positive CEGIO-S Prec 3  ##############"	
 
-	echo "#############  CBMC - MINISAT  ##############"	
+	echo "#############  CBMC - MINISAT  ##############"	>> resultado_exp_CEGIO_G_MINISAT.txt
+	echo "#############  CBMC - MINISAT  ##############"
 	for i in "${functions[@]}"
 	do		
 		line=$i
 		func=${line//,*}       	
 
-		echo
-		(time ./optCE $func --cbmc --minisat --positive --library=math2.h --precision=2 >> resultado_exp_CEGIO_S_MINISAT.txt) 
-		echo
+		(time ./optCE_1000 $func --cbmc --minisat --positive --library=math2.h --precision=3) &>> resultado_exp_CEGIO_G_MINISAT.txt
 	done
 
+	echo "#############  ESBMC - Mathsat  ##############"	>> resultado_exp_CEGIO_G_MINISAT.txt	
 	echo "#############  ESBMC - Mathsat  ##############"	
 	for i in "${functions[@]}"
 	do		
 		line=$i
 		func=${line//,*}       	
 
-		echo
-		(time ./optCE $func --esbmc --mathsat --positive --library=math2.h --precision=2 >> resultado_exp_CEGIO_S_MATHSAT.txt) 
-		echo
+		(time ./optCE_1000 $func --esbmc --mathsat --positive --library=math2.h --precision=3) &>> resultado_exp_CEGIO_G_MATHSAT.txt 
 	done
 
-
+	echo "#############  ESBMC - Boolector  ##############"	>> resultado_exp_CEGIO_G_MINISAT.txt
 	echo "#############  ESBMC - Boolector  ##############"	
 	for i in "${functions[@]}"
 	do		
 		line=$i
 		func=${line//,*}       	
 
-		echo
-		(time ./optCE $func --esbmc --boolector --positive --library=math2.h --precision=2 >> resultado_exp_CEGIO_S_BOOLECTOR.txt) 
-		echo
+		(time ./optCE_1000 $func --esbmc --boolector --positive --library=math2.h --precision=3) &>> resultado_exp_CEGIO_G_BOOLECTOR.txt 
 	done
 
+	echo "#############  ESBMC - Z3  ##############" >> resultado_exp_CEGIO_G_MINISAT.txt
 	echo "#############  ESBMC - Z3  ##############"	
 	for i in "${functions[@]}"
 	do		
 		line=$i
 		func=${line//,*}       	
 
-		echo
-		(time ./optCE $func --esbmc --z3 --positive --library=math2.h --precision=2 >> resultado_exp_CEGIO_S_Z3.txt) 
-		echo
+		(time ./optCE_1000 $func --esbmc --z3 --positive --library=math2.h --precision=3) &>> resultado_exp_CEGIO_G_Z3.txt 
 	done
 
